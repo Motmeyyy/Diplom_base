f8e3499b9ee0737c2d339ba83ccc528286ac0f8e
m48a3 <ivankov2001@gmail.com>
2023-06-01 19:25:05 +0300

    Добавил новые страницы в навбаре для пациента и переходы на них

--- step2/my_site/blog/urls.py
+++ step2/my_site/blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('med/', views.med_list, name='med-list'),
     path('med/<int:user_id>/', views.user_detail, name='user-detail'),
     path('verify/', verify, name='verify'),
+    path('my_health/', views.my_health, name='my-health'),
+    path('my_appointments/', views.my_appointments, name='my-appointments'),
+    path('make_appointment/', views.make_appointment, name='make-appointment'),
 ]
