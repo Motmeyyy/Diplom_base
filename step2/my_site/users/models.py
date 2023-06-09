ba55021f183cacd9267da4fad2ff1afb38daf6eb
m48a3 <ivankov2001@gmail.com>
2023-06-09 17:52:35 +0300

    Коммит для чатгпт

--- step2/my_site/users/models.py
+++ step2/my_site/users/models.py
@@ -1,3 +1,63 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+#
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+#     full_name = models.CharField(max_length=255,default = 'FIO')
+#     polis = models.CharField(max_length=20, blank=True, null=True)
+#     phone_number = models.CharField(max_length=20, blank=True, null=True)
+#     heart_rate = models.IntegerField(blank=True, null=True)
+#     diet = models.CharField(max_length=255, blank=True, null=True)
+#     date_of_birth = models.DateField(blank=True, null=True)
+#     address = models.CharField(max_length=255, blank=True, null=True)
+#     medical_history = models.TextField(blank=True, null=True)
+#     email_key = models.CharField(max_length=255, default= 'Nz2cNLSLUb3UhKf6eQvb')
+#     email_sender = models.CharField(max_length=255, default='ivankov2001@gmail.com')
+#     def __str__(self):
+#         return f'{self.user.username} Profile'
+#
+#
+# class VerificationRequest(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     is_verified = models.BooleanField(default=False)
+#     verification_code = models.CharField(max_length=20)
+#
+# class Appointment(models.Model):
+#     patient = models.ForeignKey(User, on_delete=models.CASCADE)
+#     doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
+#     date = models.DateTimeField()
+#     description = models.TextField()
+#
+#     def __str__(self):
+#         return f"Appointment for {self.patient.username} with {self.doctor.username} at {self.date}"
+#
+# class PurchaseHistory(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     item = models.CharField(max_length=255)
+#     receipt_hash = models.CharField(max_length=32, unique=True)
+#     diet = models.ForeignKey('Diet', on_delete=models.SET_NULL, blank=True, null=True)  # Поле для связи с диетой
+#
+#     def __str__(self):
+#         return f'{self.user.username} - {self.item}'
+#
+# class Diet(models.Model):
+#     name = models.CharField(max_length=255)
+#     description = models.TextField()
+#     recipes = models.ManyToManyField('Recipe')
+#
+#     def __str__(self):
+#         return self.name
+#
+# class Recipe(models.Model):
+#     name = models.CharField(max_length=255)
+#     ingredients = models.ManyToManyField('PurchaseHistory')
+#     # Другие поля, связанные с рецептом
+#
+#     def __str__(self):
+#         return self.name
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,7 +65,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    full_name = models.CharField(max_length=255,default = 'FIO')
+    full_name = models.CharField(max_length=255, default='FIO')
     polis = models.CharField(max_length=20, blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     heart_rate = models.IntegerField(blank=True, null=True)
@@ -13,8 +73,9 @@ class Profile(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
     medical_history = models.TextField(blank=True, null=True)
-    email_key = models.CharField(max_length=255, default= 'Nz2cNLSLUb3UhKf6eQvb')
+    email_key = models.CharField(max_length=255, default='Nz2cNLSLUb3UhKf6eQvb')
     email_sender = models.CharField(max_length=255, default='ivankov2001@gmail.com')
+
     def __str__(self):
         return f'{self.user.username} Profile'
 
@@ -24,6 +85,7 @@ class VerificationRequest(models.Model):
     is_verified = models.BooleanField(default=False)
     verification_code = models.CharField(max_length=20)
 
+
 class Appointment(models.Model):
     patient = models.ForeignKey(User, on_delete=models.CASCADE)
     doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointments')
@@ -32,3 +94,31 @@ class Appointment(models.Model):
 
     def __str__(self):
         return f"Appointment for {self.patient.username} with {self.doctor.username} at {self.date}"
+
+
+class PurchaseHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.CharField(max_length=255)
+    receipt_hash = models.CharField(max_length=32, unique=True)
+    diet = models.ForeignKey('Diet', on_delete=models.SET_NULL, blank=True, null=True)  # Поле для связи с диетой
+
+    def __str__(self):
+        return f'{self.user.username} - {self.item}'
+
+
+class Diet(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    recipes = models.ManyToManyField('Recipe')
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    ingredients = models.ManyToManyField('PurchaseHistory')
+    # Другие поля, связанные с рецептом
+
+    def __str__(self):
+        return self.name
